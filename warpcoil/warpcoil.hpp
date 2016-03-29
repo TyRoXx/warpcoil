e69cff15249670ade5348b96ed997bd5b49cf828
TyRoXx <tyroxxxx@gmail.com>
2016-03-29 20:33:33 +0200

    generate C++ types and functions from a custom data structure

--- warpcoil/warpcoil.hpp
+++ warpcoil/warpcoil.hpp
@@ -1,5 +1,101 @@
 #pragma once
 
+#include <silicium/variant.hpp>
+#include <map>
+
 namespace warpcoil
 {
+	namespace expressions
+	{
+		typedef std::string identifier;
+
+		struct call;
+		struct tuple;
+		struct closure;
+		struct literal;
+		struct tuple_element;
+
+		typedef Si::variant<std::unique_ptr<call>, std::unique_ptr<tuple>, std::unique_ptr<closure>,
+		                    std::unique_ptr<literal>, identifier, std::unique_ptr<tuple_element>> expression;
+
+		struct call
+		{
+			expression callee;
+			expression argument;
+		};
+
+		struct tuple
+		{
+			std::vector<expression> elements;
+		};
+
+		struct closure
+		{
+			expression result;
+		};
+
+		struct tuple_element
+		{
+			expression tuple;
+			std::size_t element;
+		};
+	}
+
+	namespace values
+	{
+		typedef std::uint64_t integer;
+
+		struct closure;
+		struct tuple;
+
+		typedef Si::variant<integer, std::unique_ptr<closure>, std::unique_ptr<tuple>> value;
+
+		struct closure
+		{
+			std::map<expressions::identifier, value> bound;
+			expressions::expression result;
+		};
+
+		struct tuple
+		{
+			std::vector<value> elements;
+		};
+	}
+
+	namespace expressions
+	{
+		struct literal
+		{
+			values::value value;
+		};
+	}
+
+	namespace types
+	{
+		struct integer
+		{
+		};
+
+		struct variant;
+		struct tuple;
+		struct subset;
+
+		typedef Si::variant<integer, std::unique_ptr<variant>, std::unique_ptr<tuple>, std::unique_ptr<subset>> type;
+
+		struct variant
+		{
+			std::vector<type> elements;
+		};
+
+		struct tuple
+		{
+			std::vector<type> elements;
+		};
+
+		struct subset
+		{
+			type superset;
+			values::closure is_inside;
+		};
+	}
 }
