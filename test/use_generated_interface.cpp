8f5529a4d6c1318701b70d43c45a5d7ab53955f6
TyRoXx <tyroxxxx@gmail.com>
2016-04-04 22:00:58 +0200

    test all test_interface client-side methods

--- test/use_generated_interface.cpp
+++ test/use_generated_interface.cpp
@@ -65,25 +65,69 @@ BOOST_AUTO_TEST_CASE(two_parameters)
 	BOOST_CHECK_EQUAL(20, t.two_parameters(std::make_tuple(4, 5)));
 }
 
-BOOST_AUTO_TEST_CASE(serialization_client)
+template <class Argument, class ExpectedResult, class ExpectedRequest,
+          class Response>
+void generic_test_serialization_client(
+    ExpectedResult (test_interface::*call)(Argument), Argument argument,
+    ExpectedResult expected_result, ExpectedRequest expected_request,
+    Response response)
 {
 	std::vector<std::uint8_t> request;
 	auto request_writer = Si::Sink<std::uint8_t, Si::success>::erase(
 	    Si::make_container_sink(request));
-	std::array<std::uint8_t, 8> const response = {
-	    {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88}};
 	auto response_reader =
 	    Si::Source<std::uint8_t>::erase(Si::make_container_source(response));
 	test_interface_client t(request_writer, response_reader);
-	BOOST_CHECK_EQUAL(0x1122334455667788ULL,
-	                  t.two_parameters(std::make_tuple(4, 5)));
-	std::array<std::uint8_t, 1 + 14 + (2 * 8)> const expected_request = {
-	    {14, 't', 'w', 'o', '_', 'p', 'a', 'r', 'a', 'm', 'e', 't', 'e', 'r',
-	     's', 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 5}};
+	BOOST_CHECK(expected_result == (t.*call)(argument));
 	BOOST_CHECK_EQUAL_COLLECTIONS(expected_request.begin(),
 	                              expected_request.end(), request.begin(),
 	                              request.end());
 	BOOST_CHECK_EQUAL(Si::none, Si::get(response_reader));
+}
+
+BOOST_AUTO_TEST_CASE(serialization_client_no_result_no_parameter)
+{
+	generic_test_serialization_client(
+	    &test_interface::no_result_no_parameter, std::tuple<>(), std::tuple<>(),
+	    std::array<std::uint8_t, 1 + 22>{
+	        {22, 'n', 'o', '_', 'r', 'e', 's', 'u', 'l', 't', '_', 'n', 'o',
+	         '_', 'p', 'a', 'r', 'a', 'm', 'e', 't', 'e', 'r'}},
+	    std::array<std::uint8_t, 0>{{}});
+}
+
+BOOST_AUTO_TEST_CASE(serialization_client_two_parameters)
+{
+	generic_test_serialization_client(
+	    &test_interface::two_parameters,
+	    std::tuple<std::uint64_t, std::uint64_t>(2, 3), std::uint64_t(6),
+	    std::array<std::uint8_t, 1 + 14 + (2 * 8)>{
+	        {14, 't', 'w', 'o', '_', 'p', 'a', 'r', 'a', 'm', 'e', 't', 'e',
+	         'r', 's', 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3}},
+	    std::array<std::uint8_t, 8>{{0, 0, 0, 0, 0, 0, 0, 6}});
+}
+
+BOOST_AUTO_TEST_CASE(serialization_client_two_results)
+{
+	generic_test_serialization_client(
+	    &test_interface::two_results, std::uint64_t(6),
+	    std::tuple<std::uint64_t, std::uint64_t>(6, 7),
+	    std::array<std::uint8_t, 1 + 11 + 8>{{11, 't', 'w', 'o', '_', 'r', 'e',
+	                                          's', 'u', 'l', 't', 's', 0, 0, 0,
+	                                          0, 0, 0, 0, 6}},
+	    std::array<std::uint8_t, 2 * 8>{
+	        {0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 7}});
+}
+
+BOOST_AUTO_TEST_CASE(serialization_client_vectors)
+{
+	generic_test_serialization_client(
+	    &test_interface::vectors, std::vector<std::uint64_t>{4, 5},
+	    std::vector<std::uint64_t>{5, 4},
+	    std::array<std::uint8_t, 1 + 7 + (3 * 8)>{
+	        {7, 'v', 'e', 'c', 't', 'o', 'r', 's', 0, 0, 0, 0, 0, 0, 0, 2, 0, 0,
+	         0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 5}},
+	    std::array<std::uint8_t, 3 * 8>{{0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0,
+	                                     0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 4}});
 }
 
 BOOST_AUTO_TEST_CASE(serialization_server)
@@ -102,26 +146,4 @@ BOOST_AUTO_TEST_CASE(serialization_server)
 	BOOST_CHECK_EQUAL_COLLECTIONS(expected_response.begin(),
 	                              expected_response.end(), response.begin(),
 	                              response.end());
-}
-
-BOOST_AUTO_TEST_CASE(test_vector)
-{
-	std::vector<std::uint8_t> request;
-	auto request_writer = Si::Sink<std::uint8_t, Si::success>::erase(
-	    Si::make_container_sink(request));
-	std::array<std::uint8_t, 24> const response = {{0, 0, 0, 0, 0, 0, 0, 2, 0,
-	                                                0, 0, 0, 0, 0, 0, 3, 0, 0,
-	                                                0, 0, 0, 0, 0, 4}};
-	auto response_reader =
-	    Si::Source<std::uint8_t>::erase(Si::make_container_source(response));
-	test_interface_client t(request_writer, response_reader);
-	BOOST_CHECK((std::vector<std::uint64_t>{3, 4}) ==
-	            t.vectors(std::vector<std::uint64_t>{4, 3}));
-	std::array<std::uint8_t, 1 + 7 + 24> const expected_request = {
-	    {7, 'v', 'e', 'c', 't', 'o', 'r', 's', 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0,
-	     0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 3}};
-	BOOST_CHECK_EQUAL_COLLECTIONS(expected_request.begin(),
-	                              expected_request.end(), request.begin(),
-	                              request.end());
-	BOOST_CHECK_EQUAL(Si::none, Si::get(response_reader));
 }
