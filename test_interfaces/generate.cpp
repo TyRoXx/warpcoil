921904a5a262bf0f6380cb6cf8f1f34afe71bd67
TyRoXx <tyroxxxx@gmail.com>
2016-04-16 14:35:42 +0200

    update the generated header file only if it changed

--- test_interfaces/generate.cpp
+++ test_interfaces/generate.cpp
@@ -1,4 +1,6 @@
 #include <silicium/sink/iterator_sink.hpp>
+#include <silicium/source/memory_source.hpp>
+#include <ventura/run_process.hpp>
 #include <warpcoil/cpp/generate/everything.hpp>
 #include <warpcoil/update_generated_file.hpp>
 
@@ -61,6 +63,39 @@ int main(int argc, char **argv)
 		                                        Si::to_unique(types::vector{types::integer(), types::integer()})}));
 		cpp::generate_serializable_interface(code_writer, top_level, Si::make_c_str_range("test_interface"),
 		                                     definition);
+	}
+	Si::optional<ventura::absolute_path> const clang_format =
+	    (argc >= 3) ? ventura::absolute_path::create(argv[2]) : Si::none;
+	Si::optional<ventura::absolute_path> const clang_format_config_dir =
+	    (argc >= 4) ? ventura::absolute_path::create(argv[3]) : Si::none;
+	if (clang_format)
+	{
+		if (!clang_format_config_dir)
+		{
+			std::cerr << "command line argument for the clang-format config directory missing\n";
+			return 1;
+		}
+		std::vector<char> formatted_code;
+		auto formatted_code_writer = Si::Sink<char, Si::success>::erase(Si::make_container_sink(formatted_code));
+		ventura::process_parameters parameters;
+		parameters.executable = *clang_format;
+		parameters.current_path = *clang_format_config_dir;
+		parameters.out = &formatted_code_writer;
+		auto original_code_reader = Si::Source<char>::erase(Si::make_container_source(code));
+		parameters.in = &original_code_reader;
+		parameters.inheritance = ventura::environment_inheritance::inherit;
+		Si::error_or<int> result = ventura::run_process(parameters);
+		if (result.is_error())
+		{
+			std::cerr << "clang-format failed: " << result.error() << '\n';
+			return 1;
+		}
+		if (result.get() != 0)
+		{
+			std::cerr << "clang-format failed with return code " << result.get() << '\n';
+			return 1;
+		}
+		code = std::move(formatted_code);
 	}
 	return warpcoil::update_generated_file(argv[1], Si::make_contiguous_range(code), std::cerr) ? 0 : 1;
 }
