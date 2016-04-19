82f25d442f6fe0c8c9cbbf2d2b6be1765c368ef4
TyRoXx <tyroxxxx@gmail.com>
2016-04-19 21:22:20 +0200

    remove redundant test

--- test/asynchronous.cpp
+++ test/asynchronous.cpp
@@ -46,48 +46,6 @@ BOOST_AUTO_TEST_CASE(async_client_with_callback)
 	BOOST_CHECK(ok2);
 }
 
-BOOST_AUTO_TEST_CASE(async_client_with_asio_spawn)
-{
-	boost::asio::io_service io;
-	boost::asio::ip::tcp::acceptor acceptor(io, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v6(), 0), true);
-	acceptor.listen();
-	boost::asio::ip::tcp::socket accepted_socket(io);
-	std::array<std::uint8_t, 1024> receive_buffer;
-	bool ok1 = false;
-	acceptor.async_accept(accepted_socket, [&accepted_socket, &receive_buffer, &ok1](boost::system::error_code ec)
-	                      {
-		                      BOOST_REQUIRE_EQUAL(boost::system::error_code(), ec);
-		                      accepted_socket.async_receive(
-		                          boost::asio::buffer(receive_buffer),
-		                          [&accepted_socket, &ok1](boost::system::error_code ec, std::size_t received)
-		                          {
-			                          BOOST_REQUIRE_EQUAL(boost::system::error_code(), ec);
-			                          BOOST_REQUIRE_EQUAL(23, received);
-			                          BOOST_REQUIRE(!ok1);
-			                          ok1 = true;
-			                      });
-		                  });
-	boost::asio::ip::tcp::socket socket(io);
-	async_test_interface_client<boost::asio::ip::tcp::socket, boost::asio::ip::tcp::socket> client(socket, socket);
-	bool ok2 = false;
-	socket.async_connect(
-	    boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4::loopback(), acceptor.local_endpoint().port()),
-	    [&io, &client, &ok2](boost::system::error_code ec)
-	    {
-		    BOOST_REQUIRE_EQUAL(boost::system::error_code(), ec);
-		    boost::asio::spawn(io, [&client, &ok2](boost::asio::yield_context yield)
-		                       {
-			                       std::tuple<> result = client.no_result_no_parameter(std::tuple<>(), yield);
-			                       Si::ignore_unused_variable_warning(result);
-			                       BOOST_REQUIRE(!ok2);
-			                       ok2 = true;
-			                   });
-		});
-	io.run();
-	BOOST_CHECK(ok1);
-	BOOST_CHECK(ok2);
-}
-
 namespace
 {
 	struct impl_test_interface : async_test_interface
