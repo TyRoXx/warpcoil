34ee62b9c5c273e2413966af8005339b0da4ecd6
TyRoXx <tyroxxxx@gmail.com>
2016-04-23 15:15:04 +0200

    disable VC++ warnings in beast

--- example/hello_websocket.cpp
+++ example/hello_websocket.cpp
@@ -2,7 +2,15 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <silicium/error_or.hpp>
 #include <iostream>
+
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable: 4100 4701 4127 4706 4267 4324)
+#endif
 #include <beast/websocket.hpp>
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
 
 namespace server
 {
