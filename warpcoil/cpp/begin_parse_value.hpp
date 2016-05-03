24930d87e1018cb0b782d798ca9f58c6cdd3e207
TyRoXx <tyroxxxx@gmail.com>
2016-05-03 21:04:51 +0200

    begin_parse_value uses asio_handler_invoke

--- warpcoil/cpp/begin_parse_value.hpp
+++ warpcoil/cpp/begin_parse_value.hpp
@@ -2,59 +2,96 @@
 
 #include <warpcoil/cpp/helpers.hpp>
 #include <boost/asio/buffer.hpp>
+#include <boost/asio/handler_invoke_hook.hpp>
 
 namespace warpcoil
 {
     namespace cpp
     {
         template <class AsyncReadStream, class Buffer, class Parser, class ResultHandler>
+        struct parse_operation
+        {
+            explicit parse_operation(AsyncReadStream &input, Buffer receive_buffer, std::size_t &receive_buffer_used,
+                                     Parser parser, ResultHandler on_result)
+                : m_input(input)
+                , m_receive_buffer(receive_buffer)
+                , m_receive_buffer_used(receive_buffer_used)
+                , m_parser(std::move(parser))
+                , m_on_result(std::move(on_result))
+            {
+            }
+
+            void start() &&
+            {
+                using boost::asio::asio_handler_invoke;
+                for (std::size_t i = 0; i < m_receive_buffer_used; ++i)
+                {
+                    std::uint8_t *const data = boost::asio::buffer_cast<std::uint8_t *>(m_receive_buffer);
+                    parse_result<typename Parser::result_type> response = m_parser.parse_byte(data[i]);
+                    if (Si::visit<bool>(
+                            response,
+                            [&](typename Parser::result_type &message)
+                            {
+                                std::copy(data + i + 1, data + m_receive_buffer_used, data);
+                                m_receive_buffer_used -= 1 + i;
+                                asio_handler_invoke(
+                                    std::bind(std::ref(m_on_result), boost::system::error_code(), std::move(message)),
+                                    &m_on_result);
+                                return true;
+                            },
+                            [](need_more_input)
+                            {
+                                return false;
+                            },
+                            [&](invalid_input)
+                            {
+                                asio_handler_invoke(std::bind(std::ref(m_on_result), make_invalid_input_error(),
+                                                              typename Parser::result_type{}),
+                                                    &m_on_result);
+                                return true;
+                            }))
+                    {
+                        return;
+                    }
+                }
+                m_input.async_read_some(m_receive_buffer, std::move(*this));
+            }
+
+            void operator()(boost::system::error_code ec, std::size_t read)
+            {
+                if (!!ec)
+                {
+                    using boost::asio::asio_handler_invoke;
+                    asio_handler_invoke(std::bind(std::ref(m_on_result), ec, typename Parser::result_type{}),
+                                        &m_on_result);
+                    return;
+                }
+                m_receive_buffer_used = read;
+                std::move(*this).start();
+            }
+
+            template <class Function>
+            friend void asio_handler_invoke(Function &&f, parse_operation *operation)
+            {
+                using boost::asio::asio_handler_invoke;
+                asio_handler_invoke(f, &operation->m_on_result);
+            }
+
+        private:
+            AsyncReadStream &m_input;
+            Buffer m_receive_buffer;
+            std::size_t &m_receive_buffer_used;
+            Parser m_parser;
+            ResultHandler m_on_result;
+        };
+
+        template <class AsyncReadStream, class Buffer, class Parser, class ResultHandler>
         void begin_parse_value(AsyncReadStream &input, Buffer receive_buffer, std::size_t &receive_buffer_used,
                                Parser parser, ResultHandler &&on_result)
         {
-            for (std::size_t i = 0; i < receive_buffer_used; ++i)
-            {
-                std::uint8_t *const data = boost::asio::buffer_cast<std::uint8_t *>(receive_buffer);
-                parse_result<typename Parser::result_type> response = parser.parse_byte(data[i]);
-                if (Si::visit<bool>(response,
-                                    [&](typename Parser::result_type &message)
-                                    {
-                                        std::copy(data + i + 1, data + receive_buffer_used, data);
-                                        receive_buffer_used -= 1 + i;
-                                        std::forward<ResultHandler>(on_result)(boost::system::error_code(),
-                                                                               std::move(message));
-                                        return true;
-                                    },
-                                    [](need_more_input)
-                                    {
-                                        return false;
-                                    },
-                                    [&](invalid_input)
-                                    {
-                                        std::forward<ResultHandler>(on_result)(make_invalid_input_error(), {});
-                                        return true;
-                                    }))
-                {
-                    return;
-                }
-            }
-            input.async_read_some(receive_buffer,
-                                  [
-                                    &input,
-                                    receive_buffer,
-                                    &receive_buffer_used,
-                                    parser = std::move(parser),
-                                    on_result = std::forward<ResultHandler>(on_result)
-                                  ](boost::system::error_code ec, std::size_t read) mutable
-                                  {
-                                      if (!!ec)
-                                      {
-                                          std::forward<ResultHandler>(on_result)(ec, {});
-                                          return;
-                                      }
-                                      receive_buffer_used = read;
-                                      begin_parse_value(input, receive_buffer, receive_buffer_used, std::move(parser),
-                                                        std::forward<ResultHandler>(on_result));
-                                  });
+            parse_operation<AsyncReadStream, Buffer, Parser, typename std::decay<ResultHandler>::type> operation(
+                input, receive_buffer, receive_buffer_used, std::move(parser), std::forward<ResultHandler>(on_result));
+            std::move(operation).start();
         }
     }
 }
