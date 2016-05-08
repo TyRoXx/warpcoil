9347965b3ad1952efbbea74b85d144f1f878e236
TyRoXx <tyroxxxx@gmail.com>
2016-05-08 19:37:38 +0200

    move stuff into namespace detail

--- warpcoil/cpp/request_operation.hpp
+++ warpcoil/cpp/request_operation.hpp
@@ -9,91 +9,94 @@ namespace warpcoil
 {
     namespace cpp
     {
-        template <class ResultHandler, class Result>
-        struct response_parse_operation
+        namespace detail
         {
-            explicit response_parse_operation(ResultHandler handler)
-                : m_handler(std::move(handler))
+            template <class ResultHandler, class Result>
+            struct response_parse_operation
             {
-            }
+                explicit response_parse_operation(ResultHandler handler)
+                    : m_handler(std::move(handler))
+                {
+                }
 
-            void operator()(boost::system::error_code ec, Result result)
+                void operator()(boost::system::error_code ec, Result result)
+                {
+                    m_handler(ec, std::move(result));
+                }
+
+            private:
+                ResultHandler m_handler;
+
+                template <class Function>
+                friend void asio_handler_invoke(Function &&f, response_parse_operation *operation)
+                {
+                    using boost::asio::asio_handler_invoke;
+                    asio_handler_invoke(f, &operation->m_handler);
+                }
+            };
+
+            template <class ResultHandler, class AsyncReadStream, class Buffer, class Parser>
+            struct request_send_operation
             {
-                m_handler(ec, std::move(result));
-            }
+                explicit request_send_operation(ResultHandler handler, AsyncReadStream &input, Buffer receive_buffer,
+                                                std::size_t &receive_buffer_used)
+                    : m_handler(std::move(handler))
+                    , m_input(input)
+                    , m_receive_buffer(receive_buffer)
+                    , m_receive_buffer_used(receive_buffer_used)
+                {
+                }
 
-        private:
-            ResultHandler m_handler;
+                void operator()(boost::system::error_code ec, std::size_t)
+                {
+                    if (!!ec)
+                    {
+                        m_handler(ec, {});
+                        return;
+                    }
+                    begin_parse_value(
+                        m_input, m_receive_buffer, m_receive_buffer_used, Parser(),
+                        response_parse_operation<ResultHandler, typename Parser::result_type>(std::move(m_handler)));
+                }
 
-            template <class Function>
-            friend void asio_handler_invoke(Function &&f, response_parse_operation *operation)
+            private:
+                ResultHandler m_handler;
+                AsyncReadStream &m_input;
+                Buffer m_receive_buffer;
+                std::size_t &m_receive_buffer_used;
+
+                template <class Function>
+                friend void asio_handler_invoke(Function &&f, request_send_operation *operation)
+                {
+                    using boost::asio::asio_handler_invoke;
+                    asio_handler_invoke(f, &operation->m_handler);
+                }
+            };
+
+            template <class ResultHandler>
+            struct no_response_request_send_operation
             {
-                using boost::asio::asio_handler_invoke;
-                asio_handler_invoke(f, &operation->m_handler);
-            }
-        };
+                explicit no_response_request_send_operation(ResultHandler handler)
+                    : m_handler(std::move(handler))
+                {
+                }
 
-        template <class ResultHandler, class AsyncReadStream, class Buffer, class Parser>
-        struct request_send_operation
-        {
-            explicit request_send_operation(ResultHandler handler, AsyncReadStream &input, Buffer receive_buffer,
-                                            std::size_t &receive_buffer_used)
-                : m_handler(std::move(handler))
-                , m_input(input)
-                , m_receive_buffer(receive_buffer)
-                , m_receive_buffer_used(receive_buffer_used)
-            {
-            }
-
-            void operator()(boost::system::error_code ec, std::size_t)
-            {
-                if (!!ec)
+                void operator()(boost::system::error_code ec, std::size_t)
                 {
                     m_handler(ec, {});
-                    return;
                 }
-                begin_parse_value(
-                    m_input, m_receive_buffer, m_receive_buffer_used, Parser(),
-                    response_parse_operation<ResultHandler, typename Parser::result_type>(std::move(m_handler)));
-            }
 
-        private:
-            ResultHandler m_handler;
-            AsyncReadStream &m_input;
-            Buffer m_receive_buffer;
-            std::size_t &m_receive_buffer_used;
+            private:
+                ResultHandler m_handler;
 
-            template <class Function>
-            friend void asio_handler_invoke(Function &&f, request_send_operation *operation)
-            {
-                using boost::asio::asio_handler_invoke;
-                asio_handler_invoke(f, &operation->m_handler);
-            }
-        };
-
-        template <class ResultHandler>
-        struct no_response_request_send_operation
-        {
-            explicit no_response_request_send_operation(ResultHandler handler)
-                : m_handler(std::move(handler))
-            {
-            }
-
-            void operator()(boost::system::error_code ec, std::size_t)
-            {
-                m_handler(ec, {});
-            }
-
-        private:
-            ResultHandler m_handler;
-
-            template <class Function>
-            friend void asio_handler_invoke(Function &&f, no_response_request_send_operation *operation)
-            {
-                using boost::asio::asio_handler_invoke;
-                asio_handler_invoke(f, &operation->m_handler);
-            }
-        };
+                template <class Function>
+                friend void asio_handler_invoke(Function &&f, no_response_request_send_operation *operation)
+                {
+                    using boost::asio::asio_handler_invoke;
+                    asio_handler_invoke(f, &operation->m_handler);
+                }
+            };
+        }
 
         enum class client_pipeline_request_status
         {
@@ -129,9 +132,10 @@ namespace warpcoil
                 auto receive_buffer = boost::asio::buffer(response_buffer);
                 boost::asio::async_write(
                     requests, boost::asio::buffer(request_buffer),
-                    warpcoil::cpp::request_send_operation<typename std::decay<decltype(handler)>::type, AsyncReadStream,
-                                                          decltype(receive_buffer), ResultParser>(
-                        std::move(handler), responses, receive_buffer, response_buffer_used));
+                    warpcoil::cpp::detail::request_send_operation<typename std::decay<decltype(handler)>::type,
+                                                                  AsyncReadStream, decltype(receive_buffer),
+                                                                  ResultParser>(std::move(handler), responses,
+                                                                                receive_buffer, response_buffer_used));
             }
 
             template <class ResultParser, class RequestBuilder, class ResultHandler>
@@ -149,10 +153,9 @@ namespace warpcoil
                         return;
                     }
                 }
-                boost::asio::async_write(
-                    requests, boost::asio::buffer(request_buffer),
-                    warpcoil::cpp::no_response_request_send_operation<typename std::decay<decltype(handler)>::type>(
-                        std::move(handler)));
+                boost::asio::async_write(requests, boost::asio::buffer(request_buffer),
+                                         warpcoil::cpp::detail::no_response_request_send_operation<
+                                             typename std::decay<decltype(handler)>::type>(std::move(handler)));
             }
 
         private:
