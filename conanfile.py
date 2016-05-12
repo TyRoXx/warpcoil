990e702e8f133c99c9e4c27bb3c2165d3b1880a7
TyRoXx <tyroxxxx@gmail.com>
2016-05-12 21:01:00 +0200

    correct the link to Github

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class WarpcoilConan(ConanFile):
     version = "0.1"
     generators = "cmake"
     requires = "ventura/0.5@TyRoXx/master", "utfcpp/2.3.4@TyRoXx/stable", "Beast/2016.5.10@TyRoXx/master"
-    url="http://github.com/tyroxx/warpcoil"
+    url="https://github.com/TyRoXx/warpcoil"
     license="MIT"
     exports="warpcoil/*"
 
