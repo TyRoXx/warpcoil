b31c3016e6ecefc5fa811fdea1a1b9d965869338
TyRoXx <tyroxxxx@gmail.com>
2016-05-16 12:28:22 +0200

    upgrade silicium to 0.11

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ class WarpcoilConan(ConanFile):
     name = "warpcoil"
     version = "0.1"
     generators = "cmake"
-    requires = "ventura/0.5@TyRoXx/master", "utfcpp/2.3.4@TyRoXx/stable", "Beast/2016.5.10@TyRoXx/master"
+    requires = "ventura/0.5@TyRoXx/master", "utfcpp/2.3.4@TyRoXx/stable", "Beast/2016.5.10@TyRoXx/master", "silicium/0.11@TyRoXx/master"
     url="https://github.com/TyRoXx/warpcoil"
     license="MIT"
     exports="warpcoil/*"
