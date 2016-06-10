9a5df3c41940ea875da1e2bb7b133d2030015aac
TyRoXx <tyroxxxx@gmail.com>
2016-06-10 15:00:58 +0200

    test queueing of many small messages in buffered_writer

--- test/buffered_writer.cpp
+++ test/buffered_writer.cpp
@@ -78,3 +78,41 @@ BOOST_AUTO_TEST_CASE(buffered_writer_buffer_during_send)
     Si::exchange(stream.handle_result, nullptr)({}, 1);
     BOOST_REQUIRE(!stream.handle_result);
 }
+
+BOOST_AUTO_TEST_CASE(buffered_writer_queuing)
+{
+    warpcoil::async_write_stream stream;
+    warpcoil::cpp::buffered_writer<warpcoil::async_write_stream> buffered(stream);
+    BOOST_REQUIRE(!stream.handle_result);
+    buffered.async_run([](boost::system::error_code const)
+                       {
+                           BOOST_FAIL("Unexpected error");
+                       });
+    BOOST_REQUIRE(!stream.handle_result);
+    std::vector<std::uint8_t> expected;
+    std::size_t number_of_callbacks = 0;
+    for (std::uint8_t i = 23; i < 34; ++i)
+    {
+        {
+            auto sink = buffered.buffer_sink();
+            Si::append(sink, i);
+            expected.push_back(i);
+        }
+        buffered.send_buffer([&number_of_callbacks](boost::system::error_code const ec)
+                             {
+                                 ++number_of_callbacks;
+                                 BOOST_REQUIRE(!ec);
+                             });
+        BOOST_REQUIRE(stream.handle_result);
+    }
+    BOOST_REQUIRE_EQUAL(1u, stream.written.size());
+    BOOST_REQUIRE_EQUAL(expected[0], stream.written[0][0]);
+    stream.written.clear();
+    Si::exchange(stream.handle_result, nullptr)({}, 1);
+    BOOST_REQUIRE_EQUAL(1u, stream.written.size());
+    BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin() + 1, expected.end(), stream.written[0].begin(),
+                                  stream.written[0].end());
+    Si::exchange(stream.handle_result, nullptr)({}, expected.size() - 1);
+    BOOST_REQUIRE(!stream.handle_result);
+    BOOST_CHECK_EQUAL(expected.size(), number_of_callbacks);
+}
