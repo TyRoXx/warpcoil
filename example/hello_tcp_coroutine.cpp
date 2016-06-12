1e59d414a6ed6b11b66447b6427397a2d798723e
TyRoXx <tyroxxxx@gmail.com>
2016-06-12 17:12:40 +0200

    remove unnecessary shared_ptrs

--- example/hello_tcp_coroutine.cpp
+++ example/hello_tcp_coroutine.cpp
@@ -30,25 +30,24 @@ int main()
     boost::asio::spawn(
         io, [&io, &acceptor](boost::asio::yield_context yield)
         {
-            auto accepted_socket = std::make_shared<ip::tcp::socket>(io);
+            ip::tcp::socket accepted_socket(io);
             server::my_hello_service server_impl;
-            acceptor.async_accept(*accepted_socket, yield);
-            warpcoil::cpp::message_splitter<ip::tcp::socket> splitter(*accepted_socket);
-            auto writer = std::make_shared<warpcoil::cpp::buffered_writer<ip::tcp::socket>>(*accepted_socket);
+            acceptor.async_accept(accepted_socket, yield);
+            warpcoil::cpp::message_splitter<ip::tcp::socket> splitter(accepted_socket);
+            warpcoil::cpp::buffered_writer<ip::tcp::socket> writer(accepted_socket);
             async_hello_as_a_service_server<decltype(server_impl), ip::tcp::socket, ip::tcp::socket> server(
-                server_impl, splitter, *writer);
+                server_impl, splitter, writer);
             server.serve_one_request(yield);
         });
 
     // client:
     boost::asio::spawn(io, [&io, &acceptor](boost::asio::yield_context yield)
                        {
-                           auto connecting_socket = std::make_shared<ip::tcp::socket>(io);
-                           warpcoil::cpp::message_splitter<ip::tcp::socket> splitter(*connecting_socket);
-                           auto writer =
-                               std::make_shared<warpcoil::cpp::buffered_writer<ip::tcp::socket>>(*connecting_socket);
-                           async_hello_as_a_service_client<ip::tcp::socket, ip::tcp::socket> client(*writer, splitter);
-                           connecting_socket->async_connect(
+                           ip::tcp::socket connecting_socket(io);
+                           warpcoil::cpp::message_splitter<ip::tcp::socket> splitter(connecting_socket);
+                           warpcoil::cpp::buffered_writer<ip::tcp::socket> writer(connecting_socket);
+                           async_hello_as_a_service_client<ip::tcp::socket, ip::tcp::socket> client(writer, splitter);
+                           connecting_socket.async_connect(
                                ip::tcp::endpoint(ip::address_v4::loopback(), acceptor.local_endpoint().port()), yield);
                            std::string result = client.hello("Alice", yield);
                            std::cout << result << '\n';
