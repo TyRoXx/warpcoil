df1d85297d628bfc8fcdc9fe962ae56e8cb56aac
TyRoXx <tyroxxxx@gmail.com>
2016-06-26 19:25:44 +0200

    begin the GUI

--- example_web_site/index.js
+++ example_web_site/index.js
@@ -11,6 +11,7 @@ function () {
 }
 ();
 socket.binaryType = "arraybuffer";
+var client;
 socket.onopen = function (event) {
     var pending_requests = {};
     var send_bytes = function (bytes) {
@@ -18,23 +19,23 @@ socket.onopen = function (event) {
     };
     var no_server_at_the_moment = {};
     var receiver = make_receiver(pending_requests, no_server_at_the_moment, send_bytes);
-    var client = make_client(pending_requests, send_bytes);
+    client = make_client(pending_requests, send_bytes);
     socket.onmessage = function (event) {
         var view = new Uint8Array(event.data);
         for (var i = 0; i < view.length; ++i) {
             receiver(view[i]);
         }
     };
-    client.hello("abc", function (error, response) {
-        alert(response);
-        client.hello("def", function (error, response) {
-            alert(response);
-        });
+};
+socket.onerror = function (event) {};
+socket.onclose = function (event) {};
+var submit = function () {
+    if (client === undefined) {
+        return;
+    }
+    client.hello(document.getElementById("input").value, function (error, response) {
+        var log = document.getElementById("log");
+        log.value += (response + "\n");
+        log.scrollTop = log.scrollHeight;
     });
-};
-socket.onerror = function (event) {
-    alert("onerror");
-};
-socket.onclose = function (event) {
-    alert("onclose");
 };
