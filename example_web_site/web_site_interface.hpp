7e2f8445fe6369f0fb02df7ed29d7a3409e8439e
TyRoXx <tyroxxxx@gmail.com>
2016-06-29 21:31:47 +0200

    add header file to project

--- example_web_site/web_site_interface.hpp
+++ example_web_site/web_site_interface.hpp
@@ -2,11 +2,11 @@
 
 namespace warpcoil
 {
-	inline types::interface_definition create_web_site_interface()
-	{
-		types::interface_definition definition;
-		definition.add_method("hello", types::utf8{ types::integer{ 0, 255 } })("name",
-			types::utf8{ types::integer{ 0, 255 } });
-		return definition;
-	}
+    inline types::interface_definition create_web_site_interface()
+    {
+        types::interface_definition definition;
+        definition.add_method("hello", types::utf8{types::integer{0, 255}})("name",
+                                                                            types::utf8{types::integer{0, 255}});
+        return definition;
+    }
 }
