bd3f505bdd9ea8ceee5238addda0b6b987a4c4c0
TyRoXx <tyroxxxx@gmail.com>
2016-08-27 21:48:17 +0200

    remove unnecessary template definition

--- warpcoil/cpp/structure_parser.hpp
+++ warpcoil/cpp/structure_parser.hpp
@@ -5,19 +5,6 @@ namespace warpcoil
     namespace cpp
     {
         template <class Structure>
-        struct structure_parser
-        {
-            typedef Structure result_type;
-
-            parse_result<result_type> parse_byte(std::uint8_t const)
-            {
-                throw std::logic_error("to do");
-            }
-
-            Si::optional<result_type> check_for_immediate_completion() const
-            {
-                throw std::logic_error("to do");
-            }
-        };
+        struct structure_parser;
     }
 }
