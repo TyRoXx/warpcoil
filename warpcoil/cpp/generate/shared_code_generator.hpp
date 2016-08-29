96329a76371110ab87ce0be6323f1c8c9dd82ac0
TyRoXx <tyroxxxx@gmail.com>
2016-08-29 20:07:02 +0200

    specialize structure_parser in the right namespace

--- warpcoil/cpp/generate/shared_code_generator.hpp
+++ warpcoil/cpp/generate/shared_code_generator.hpp
@@ -52,64 +52,81 @@ namespace warpcoil
                           }
                       },
                       ";\n");
-                start_line(m_code, m_indentation, "template <> struct warpcoil::cpp::structure_parser<");
-                warpcoil::cpp::generate_name_for_structure(m_code, required);
-                if (required.elements.empty())
-                {
-                    Si::append(m_code, ">\n");
-                    block(m_code, m_indentation,
-                          [&](indentation_level const in_struct)
-                          {
-                              start_line(m_code, in_struct, "typedef ");
-                              warpcoil::cpp::generate_name_for_structure(m_code, required);
-                              Si::append(m_code, " result_type;\n");
-                              start_line(m_code, in_struct,
-                                         "parse_result<result_type> parse_byte(std::uint8_t const) const "
-                                         "{ return "
-                                         "warpcoil::cpp::parse_complete<result_type>{result_type{}, "
-                                         "warpcoil::cpp::input_consumption::does_not_consume}; }\n");
-                              start_line(m_code, in_struct, "Si::optional<result_type> "
-                                                            "check_for_immediate_completion() const { return "
-                                                            "result_type{}; }\n");
-                          },
-                          ";\n");
-                }
-                else
-                {
-                    Si::append(m_code, "> : warpcoil::cpp::basic_tuple_parser<warpcoil::cpp::structure_parser<");
-                    warpcoil::cpp::generate_name_for_structure(m_code, required);
-                    Si::append(m_code, ">, ");
-                    warpcoil::cpp::generate_name_for_structure(m_code, required);
-                    for (types::structure::element const &element : required.elements)
-                    {
-                        Si::append(m_code, ", ");
-                        generate_parser_type(m_code, element.what);
-                    }
-                    Si::append(m_code, ">\n ");
-                    block(m_code, m_indentation,
-                          [&](indentation_level const in_struct)
-                          {
-                              std::size_t index = 0;
-                              for (types::structure::element const &element : required.elements)
-                              {
-                                  start_line(m_code, in_struct, "");
-                                  generate_type(m_code, *this, element.what);
-                                  Si::append(m_code, " &get(");
-                                  warpcoil::cpp::generate_name_for_structure(m_code, required);
-                                  Si::append(m_code, " &result, std::integral_constant<std::size_t, ");
-                                  Si::append(m_code, boost::lexical_cast<std::string>(index));
-                                  Si::append(m_code, ">) const\n");
-                                  block(m_code, in_struct,
-                                        [&](indentation_level const in_get)
+                start_line(m_code, m_indentation, "namespace warpcoil");
+                block(m_code, m_indentation,
+                      [&](indentation_level const in_warpcoil)
+                      {
+                          start_line(m_code, m_indentation, "namespace cpp");
+                          block(m_code, in_warpcoil,
+                                [&](indentation_level const in_cpp)
+                                {
+                                    start_line(m_code, in_cpp, "template <> struct structure_parser<");
+                                    warpcoil::cpp::generate_name_for_structure(m_code, required);
+                                    if (required.elements.empty())
+                                    {
+                                        Si::append(m_code, ">\n");
+                                        block(m_code, in_cpp,
+                                              [&](indentation_level const in_struct)
+                                              {
+                                                  start_line(m_code, in_struct, "typedef ");
+                                                  warpcoil::cpp::generate_name_for_structure(m_code, required);
+                                                  Si::append(m_code, " result_type;\n");
+                                                  start_line(
+                                                      m_code, in_struct,
+                                                      "parse_result<result_type> parse_byte(std::uint8_t const) const "
+                                                      "{ return "
+                                                      "parse_complete<result_type>{result_type{}, "
+                                                      "input_consumption::does_not_consume}; }\n");
+                                                  start_line(m_code, in_struct,
+                                                             "Si::optional<result_type> "
+                                                             "check_for_immediate_completion() const { return "
+                                                             "result_type{}; }\n");
+                                              },
+                                              ";\n");
+                                    }
+                                    else
+                                    {
+                                        Si::append(m_code, "> : basic_tuple_parser<structure_parser<");
+                                        warpcoil::cpp::generate_name_for_structure(m_code, required);
+                                        Si::append(m_code, ">, ");
+                                        warpcoil::cpp::generate_name_for_structure(m_code, required);
+                                        for (types::structure::element const &element : required.elements)
                                         {
-                                            start_line(m_code, in_get, "return result.", element.name, ";\n");
-                                        },
-                                        "\n");
-                                  ++index;
-                              }
-                          },
-                          ";\n");
-                }
+                                            Si::append(m_code, ", ");
+                                            generate_parser_type(m_code, element.what);
+                                        }
+                                        Si::append(m_code, ">\n ");
+                                        block(m_code, in_cpp,
+                                              [&](indentation_level const in_struct)
+                                              {
+                                                  std::size_t index = 0;
+                                                  for (types::structure::element const &element : required.elements)
+                                                  {
+                                                      start_line(m_code, in_struct, "");
+                                                      generate_type(m_code, *this, element.what);
+                                                      Si::append(m_code, " &get(");
+                                                      warpcoil::cpp::generate_name_for_structure(m_code, required);
+                                                      Si::append(m_code,
+                                                                 " &result, std::integral_constant<std::size_t, ");
+                                                      Si::append(m_code, boost::lexical_cast<std::string>(index));
+                                                      Si::append(m_code, ">) const\n");
+                                                      block(m_code, in_struct,
+                                                            [&](indentation_level const in_get)
+                                                            {
+                                                                start_line(m_code, in_get, "return result.",
+                                                                           element.name, ";\n");
+                                                            },
+                                                            "\n");
+                                                      ++index;
+                                                  }
+                                              },
+                                              ";\n");
+                                    }
+
+                                },
+                                "\n");
+                      },
+                      "\n");
                 m_generated_structures.insert(required.clone());
             }
 
