3946d7ca65cd6efc8afdaa9ce6749dee6d73e507
TyRoXx <tyroxxxx@gmail.com>
2017-03-29 22:20:37 +0200

    make the coroutine example shorter

--- example_future/example_future.cpp
+++ example_future/example_future.cpp
@@ -7,43 +7,42 @@
 
 namespace
 {
-    void delay(std::chrono::steady_clock::duration duration, boost::asio::io_service &io,
-               boost::asio::yield_context yield)
+    namespace asio = boost::asio;
+
+    void delay(std::chrono::steady_clock::duration duration, asio::io_service &io, asio::yield_context yield)
     {
-        boost::asio::steady_timer timeout(io);
+        asio::steady_timer timeout(io);
         timeout.expires_from_now(duration);
         timeout.async_wait(yield);
     }
 
-    void try_to_connect(boost::asio::ip::tcp::endpoint server, boost::asio::io_service &io,
-                        boost::asio::yield_context yield)
+    void try_to_connect(asio::ip::tcp::endpoint server, asio::io_service &io, asio::yield_context yield)
     {
-        boost::asio::ip::tcp::socket socket(io, boost::asio::ip::tcp::v4());
-        boost::asio::steady_timer timeout(io);
-
         using warpcoil::future;
 
-        future<boost::system::error_code> connected = socket.async_connect(server, warpcoil::use_future);
+        asio::ip::tcp::socket socket(io, asio::ip::tcp::v4());
 
+        asio::steady_timer timeout(io);
         timeout.expires_from_now(std::chrono::seconds(2));
-        future<boost::system::error_code> timed_out = timeout.async_wait(warpcoil::use_future);
 
-        warpcoil::all(timed_out.then([&](boost::system::error_code)
-                                     {
-                                         socket.cancel();
-                                     }),
-                      connected.then([&](boost::system::error_code ec)
-                                     {
-                                         timeout.cancel();
-                                         if (!!ec)
-                                         {
-                                             std::cerr << server << " Failed with error " << ec << '\n';
-                                         }
-                                         else
-                                         {
-                                             std::cerr << server << " Succeeded\n";
-                                         }
-                                     }))
+        warpcoil::all(timeout.async_wait(warpcoil::use_future)
+                          .then([&](boost::system::error_code)
+                                {
+                                    socket.cancel();
+                                }),
+                      socket.async_connect(server, warpcoil::use_future)
+                          .then([&](boost::system::error_code ec)
+                                {
+                                    timeout.cancel();
+                                    if (!!ec)
+                                    {
+                                        std::cerr << server << " Failed with error " << ec << '\n';
+                                    }
+                                    else
+                                    {
+                                        std::cerr << server << " Succeeded\n";
+                                    }
+                                }))
             .async_wait(yield);
     }
 }
@@ -54,27 +53,26 @@ int main()
 #ifdef _MSC_VER
     {
         // WinSock initialization crashes with an access violation if done within a Boost coroutine, so we do it here:
-        boost::asio::ip::tcp::socket dummy(io, boost::asio::ip::tcp::v4());
+        asio::ip::tcp::socket dummy(io, asio::ip::tcp::v4());
     }
 #endif
-    boost::asio::spawn(io, [&io](boost::asio::yield_context yield)
+    boost::asio::spawn(io, [&io](asio::yield_context yield)
                        {
                            for (;;)
                            {
-                               try_to_connect(boost::asio::ip::tcp::endpoint(
-                                                  boost::asio::ip::address_v4::from_string("172.217.20.195"), 80),
-                                              io, yield);
+                               try_to_connect(
+                                   asio::ip::tcp::endpoint(asio::ip::address_v4::from_string("172.217.20.195"), 80), io,
+                                   yield);
                                delay(std::chrono::seconds(1), io, yield);
                            }
                        });
-    boost::asio::spawn(
-        io, [&io](boost::asio::yield_context yield)
-        {
-            for (;;)
-            {
-                try_to_connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4::from_string("1.2.3.4"), 80),
-                               io, yield);
-            }
-        });
+    boost::asio::spawn(io, [&io](asio::yield_context yield)
+                       {
+                           for (;;)
+                           {
+                               try_to_connect(asio::ip::tcp::endpoint(asio::ip::address_v4::from_string("1.2.3.4"), 80),
+                                              io, yield);
+                           }
+                       });
     io.run();
 }
